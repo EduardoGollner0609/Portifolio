2d0ba9882d9c078f49ce41aa9ea90f3d89d3167b
EduGollner0609 <dudugollner@gmail.com>
2025-03-05 11:21:04 -0300

    HomePage.

--- src/routes/Page/HomePage/index.tsx
+++ src/routes/Page/HomePage/index.tsx
@@ -1,7 +1,24 @@
 import './styles.css';
+import myPhoto from '../../../assets/my-photo.webp';
 
 export default function HomePage() {
     return (
-        <h1>Olá Mundo</h1>
+        <main>
+            <section id="home-page-section" className="container">
+                <div className="home-page-apresentation">
+                    <p>Olá, meu nome é</p>
+                    <h2>Eduardo S. Gollner</h2>
+                    <p>Sou um desenvolvedor Full Stack especializado em Java com Spring e TypeScript com React.
+                        Sou apaixonado por aprendizado contínuo e estou sempre me desafiando para evoluir e aprimorar minhas habilidades.
+                    </p>
+                    <a href="">
+                        Conhecer Mais
+                    </a>
+                </div>
+                <div className="home-page-photo">
+                    <img src={myPhoto} alt="" />
+                </div>
+            </section>
+        </main>
     );
 }
