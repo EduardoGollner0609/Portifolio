0f87de9d5d2a1b38d39eaec2f90dc9bc62fb926a
EduGollner0609 <dudugollner@gmail.com>
2025-03-06 08:59:17 -0300

    Adicionando links.

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,17 +1,23 @@
-import '.styles.css';
+import './styles.css';
+import githubIcon from '../../assets/github-min-icon.svg';
+import openIcon from '../../assets/open-icon.svg';
 
 type Props = {
     imgUrl: string;
     title: string;
     description: string;
     skills: string[];
+    githubUrl: string;
+    projectUrl: string;
 }
 
-export default function ProjectCard({ imgUrl, title, description, skills }: Props) {
+export default function ProjectCard({ imgUrl, title, description, skills, githubUrl, projectUrl }: Props) {
     return (
         <div className="project-card">
             <div className="project-card-img">
-                <img src={imgUrl} alt={`Foto do projeto ${title}`} />
+                <a href={githubUrl}>
+                    <img src={imgUrl} alt={`Foto do projeto ${title}`} />
+                </a>
             </div>
             <div className="project-card-details">
                 <h3>{title}</h3>
@@ -22,6 +28,14 @@ export default function ProjectCard({ imgUrl, title, description, skills }: Prop
                     {
                         skills.map(skill => <p>{skill}</p>)
                     }
+                </div>
+                <div className="project-card-details-links">
+                    <a href={githubUrl}>
+                        <img src={githubIcon} alt="" />
+                    </a>
+                    <a href={projectUrl}>
+                        <img src={openIcon} alt="" />
+                    </a>
                 </div>
             </div>
         </div>
