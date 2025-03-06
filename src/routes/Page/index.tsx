3b5106dc28edd0f444f45b0f8c4efd2cdc0d85e6
EduGollner0609 <dudugollner@gmail.com>
2025-03-06 13:43:07 -0300

    Adicionando HTML e estilizando com CSS o About Me Section.

--- src/routes/Page/index.tsx
+++ src/routes/Page/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import Header from '../../components/Header';
 import myImg from '../../assets/my-img.webp';
+import computerAnimationImg from '../../assets/computer-animation-img.svg';
 import TopTitle from '../../components/TopTitle';
 import ProjectCard from '../../components/ProjectCard';
 import gcbarbershopProjectImg from '../../assets/gcbarbershop-project-img.png';
@@ -9,6 +10,29 @@ import rossowEngenhariaProjectImg from '../../assets/rossowengenharia-project-im
 export default function Page() {
 
     const landingPageSkills = ['HTML', 'CSS', 'React'];
+
+
+    const calculateAge = (birthDate: Date) => {
+        const today = new Date();
+
+        let years = today.getFullYear() - birthDate.getFullYear();
+        let mounths = today.getMonth() - birthDate.getMonth();
+        let days = today.getDate() - birthDate.getDate();
+
+        if (days < 0) {
+            mounths--;
+            days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        }
+
+        if (mounths < 0) {
+            years--;
+            mounths += 12;
+        }
+
+        return years;
+    }
+
+    const myAge: number = calculateAge(new Date('2005-09-06'));
 
     return (
         <>
@@ -35,40 +59,79 @@ export default function Page() {
                 </section>
                 <section id="about-me-section" className="container">
                     <TopTitle title='Sobre Mim' />
-                    <div className="about-me-details">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                            Possimus sapiente,
-                            accusamus debitis recusandae rem autem iure est tempore magni inventore consequuntur
-                            cumque nesciunt maxime ea nisi voluptatem quisquam.
-                            Fuga, eligendi.
-                        </p>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                            Possimus sapiente,
-                            accusamus debitis recusandae rem autem iure est tempore magni inventore consequuntur
-                            cumque nesciunt maxime ea nisi voluptatem quisquam.
-                            Fuga, eligendi.
-                        </p>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                            Possimus sapiente,
-                            accusamus debitis recusandae rem autem iure est tempore magni inventore consequuntur
-                            cumque nesciunt maxime ea nisi voluptatem quisquam.
-                            Fuga, eligendi.
-                        </p>
-                        <div className="about-me-details-my-skills">
-                            <ul className='my-skills-list'>
-                                <li>
-                                    Java
-                                </li>
-                                <li>
-                                    Spring
-                                </li>
-                                <li>
-                                    React
-                                </li>
-                                <li>
-                                    SQL
-                                </li>
-                            </ul>
+                    <div className="about-me-content">
+                        <div className="about-me-animation-img">
+                            <img src={computerAnimationImg} alt="Animação Computador" />
+                            <div className="about-me-details-my-skills">
+                                <ul className='my-skills-list'>
+                                    <li>
+                                        Java
+                                    </li>
+                                    <li>
+                                        Spring
+                                    </li>
+                                    <li>
+                                        HTML
+                                    </li>
+                                    <li>
+                                        CSS
+                                    </li>
+                                    <li>
+                                        Typescript
+                                    </li>
+                                    <li>
+                                        React
+                                    </li>
+                                    <li>
+                                        SQL
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="about-me-details">
+                            <p>
+                                Olá, eu me chamo <strong>Eduardo Sousa Gollner</strong>,
+                                tenho {myAge} anos (nascido em 06/09/2005) e é um prazer tê-lo aqui. Se quiser saber mais sobre mim,
+                                leia com atenção.
+                            </p>
+                            <p>
+                                Sempre fui muito <strong>curioso</strong> e <strong>dedicado</strong>,
+                                gostava de saber sobre tudo e principalmente assuntos que me interessavam.
+                            </p>
+                            <p>
+                                Meu interesse por programação começou através do meu irmão,
+                                que já atuava na área e me incentivou a seguir esse caminho.
+                                Ao descobrir o poder de criar,
+                                inovar e resolver problemas com a programação,
+                                me apaixonei pela área. Comecei a investir em livros,
+                                cursos e passei muitas noites estudando.
+                                Hoje, sou um Desenvolvedor de Software Fullstack,
+                                com maior foco no BackEnd, possuo certificados:
+                                <ul>
+                                    <li>
+                                        <a href="https://www.udemy.com/certificate/UC-1b17a043-9227-4675-9a9b-65912021c184/">
+                                            Java Completo (UDEMY - 54.5 Horas)
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://devsuperior.club/c/5-3053">
+                                            Java Spring Professional (DevSuperior - 120 Horas)
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://devsuperior.club/c/18-3053">
+                                            ReactJS Professional (DevSuperior - 160 Horas)
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.udemy.com/certificate/UC-b5f6e750-1276-44ad-b4da-a92dc4113594/">
+                                            Spring Boot Expert: Jpa, REST, JWT, Oauth2 com Docker e AWS (UDEMY - 52 Horas)
+                                        </a>
+                                    </li>
+                                </ul>
+
+
+                            </p>
                         </div>
                     </div>
                 </section>
@@ -91,7 +154,7 @@ export default function Page() {
                             projectUrl='https://rossow-engenharia.netlify.app/' />
                     </div>
                 </section>
-            </main>
+            </main >
         </>
     );
 }
