bdb0e6c39ef4e6b42328fb91168a70a1f18b0650
EduGollner0609 <dudugollner@gmail.com>
2025-03-06 02:19:01 -0300

    Criando componente Header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import './styles.css';
+
+import whatsappIcon from '../../assets/whatsapp-icon.svg';
+import linkedinIcon from '../../assets/linkedin-icon.svg';
+import githubIcon from '../../assets/github-icon.svg';
+
+export default function Header() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className="header-title">
+                    <h1>Eduardo Sousa Gollner</h1>
+                    <h2>Desenvolvedor Fullstack</h2>
+                </div>
+                <div className="header-contacts">
+                    <ul className="header-list-contacts">
+                        <li className="option-contact-item">
+                            <a href="https://wa.me/5527992657127?text=Ol%C3%A1%2C%20tudo%20bem%3F%20Vim%20do%20seu%20portf%C3%B3lio.">
+                                <img src={whatsappIcon} alt="" />
+                            </a>
+                        </li>
+                        <li className="option-contact-item">
+                            <a href="https://www.linkedin.com/in/eduardo-gollner/">
+                                <img src={linkedinIcon} alt="" />
+                            </a>
+                        </li>
+                        <li className="option-contact-item">
+                            <a href="https://github.com/EduardoGollner0609">
+                                <img src={githubIcon} alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
