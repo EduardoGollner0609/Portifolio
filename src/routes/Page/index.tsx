35f92ceb921ad0157dda8272c032ca08b4e8f10b
EduGollner0609 <dudugollner@gmail.com>
2025-03-06 03:22:37 -0300

    Criando botão 'Conheça mais'

--- src/routes/Page/index.tsx
+++ src/routes/Page/index.tsx
@@ -11,12 +11,17 @@ export default function Page() {
                     <div className="apresentation-details">
                         <p>Olá, tudo bem? eu me chamo </p>
                         <h2>Eduardo Sousa Gollner</h2>
-                   
+
                         <p>
                             Sou um desenvolvedor Full Stack com experiência em Java, Spring, React e SQL.
                             Apaixonado por tecnologia, sigo boas práticas como SOLID para criar aplicações escaláveis e de alta qualidade.
                             Tenho aprendizado rápido, trabalho bem em equipe e busco sempre soluções eficientes e inovadoras. 🚀
                         </p>
+                        <div className="apresentation-btn-space">
+                            <a href="" className="apresentation-btn mt20">
+                                Conheça mais
+                            </a>
+                        </div>
                     </div>
                     <div className="apresentation-img">
                         <img src={myImg} alt="" />
